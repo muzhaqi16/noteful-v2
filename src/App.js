7d8d5e4182190bfb1931c8d49b40b06a1e9be6ec
Artan Muzhaqi <artanmuzhaqi@gmail.com>
2019-05-23 12:13:43 -0400

    convert function to class and add dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
